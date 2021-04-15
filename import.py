cede07a43b16faddb3271934e20689468179cffd
Judy Trinh <jtrinh74@gmail.com>
2021-04-14 20:58:06 -0700

    Implement most basic version of transaction import

--- import.py
+++ import.py
@@ -2,21 +2,31 @@
 
 import csv
 from datetime import datetime, timedelta
+from decimal import Decimal
+from pprint import pprint
+import requests
 import sys
 
-print(sys.argv)
+YNAB_BASE_URL = "https://api.youneedabudget.com/v1"
+BUDGET_ID = 'last-used'
 
 if len(sys.argv) < 3:
   print("ERROR: Not all required arguments were provided")
   sys.exit(1)
 
 csv_file = sys.argv[1]
-date_start = datetime.strptime(sys.argv[2],"%Y-%m-%d").date()
+access_token = sys.argv[2]
+account_id = sys.argv[3]
+date_start = datetime.strptime(sys.argv[4],"%Y-%m-%d").date()
 date_end = datetime.today().date()
 if len(sys.argv) >= 4:
-  date_end = datetime.strptime(sys.argv[3], "%Y-%m-%d").date()
+  date_end = datetime.strptime(sys.argv[5], "%Y-%m-%d").date()
 
 print(f"Importing Splitwise transactions starting from {date_start} to {date_end} inclusive from {csv_file}")
+
+data = { 
+  "transactions": []
+}
 
 with open(csv_file, newline='') as csvfile:
   reader = csv.reader(csvfile)
@@ -24,7 +34,7 @@ with open(csv_file, newline='') as csvfile:
 
   for row in reader:
     if len(row) == 0:
-      exit()
+      break
 
     txn_date = datetime.strptime(row[0], "%Y-%m-%d").date()
 
@@ -32,15 +42,26 @@ with open(csv_file, newline='') as csvfile:
     earlier_than_end = txn_date <= date_end
 
     if later_than_start and earlier_than_end:
-      description = row[4]
+      description = row[1]
       amount = row[5]
-      print(', '.join(row))
-      print(f'{row[0]}: {row[5]} {later_than_start} {earlier_than_end}')
 
-# for each date, if >= date_start and <= date_end, create the txn
+      data["transactions"].append({
+        "account_id": account_id,
+        "date": str(txn_date),
+        "amount": int(Decimal(amount) * 1000),
+        "payee_id": None,
+        "payee_name": "Eric Lee",
+        "category_id": None,
+        "memo": description,
+        "cleared": "cleared",
+        "approved": False
+      })
+      break
 
-# harcode access token
-# choose account in ynab?
-# iterate through csv lines
-# create transaction only if Judy owes eric
-
+headers = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'Authorization': f'Bearer {access_token}'
+}
+response = requests.post(f'{YNAB_BASE_URL}/budgets/{BUDGET_ID}/transactions', headers=headers, json=data)
+pprint(response.json())
